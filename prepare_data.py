3a8756e7c3333f0c31753f60d70d81ed9aa5b329
Surachai Rodsai <surachairobotic@gmail.com>
2020-03-27 00:44:36 +0700

    complete but failed

--- prepare_data.py
+++ prepare_data.py
@@ -43,6 +43,7 @@ def get_files_list(mypath):
 def add_day_label(data):
   data['date'] = pd.to_datetime(data['date'], errors='coerce')
   data['weekday'] = data['date'].dt.dayofweek
+  data['year'] = data['date'].dt.dayofyear
   return data
 
 if __name__ == '__main__':
@@ -65,8 +66,8 @@ if __name__ == '__main__':
   df_new.to_csv(r'data/humid-temp-reorder.csv', index = False, header=True)
   '''  
   '''
-  df_in = pd.read_csv(r'data/humid-temp-reorder.csv')
-  df_out = pd.read_csv(r'data/bangkok-air-quality-reorder.csv')
+  df_in = pd.read_csv(r'data/humid-temp-reorder_18-19.csv', encoding='iso-8859-1')
+  df_out = pd.read_csv(r'data/Air_Q_18-19.csv')
   
   duplicates_in = set(df_in.index).intersection(df_out.index)
   print(duplicates_in)
@@ -77,7 +78,7 @@ if __name__ == '__main__':
   
   df_in.to_csv(r'data/input.csv', index = False, header=True)
   df_out.to_csv(r'data/output.csv', index = False, header=True)
-  '''
+  '''  
     
   data_in = pd.read_csv(r'data/input.csv')
   data_out = pd.read_csv(r'data/output.csv')
@@ -85,42 +86,50 @@ if __name__ == '__main__':
   data_in = add_day_label(data_in)
   data_in = data_in.drop(data_in.columns[0], axis=1)
   data_in = data_in.drop(data_in.index[0])
-  print(data_in)
+  #print(data_in)
 
   del_column = [x for x in data_out.columns if x.find("pm25") == -1]
-  print(del_column)
-  print([data_out.columns])
-  print(type(data_out.columns))
+  #print(del_column)
+  #print([data_out.columns])
+  #print(type(data_out.columns))
 
   data_out = data_out.drop(del_column, axis=1)
   data_out = data_out.drop(data_out.index[0])
 
-  print(data_in)
-  print(data_out)
+  #print(data_in)
+  #print(data_out)
 
   cnn = MLPClassifier()
   cnn.fit(data_in, data_out.values.ravel())
-  print(cnn)
+  #print(cnn)
   
   test_in = pd.read_csv(r'data/test_in.csv')
   test_in = add_day_label(test_in)
   test_in = test_in.drop(test_in.columns[0], axis=1)
   test_in = test_in.drop(test_in.index[0])
   test_in = test_in.to_numpy()
-  print(test_in)
+  #print(test_in)
 
   Y_pred = []
   for i in range(len(test_in)):
     y_pred=(cnn.predict(test_in[[i]]))
     #print(y_pred)
     Y_pred.append(y_pred[0])
-  print(Y_pred)
+  print('predict = ', Y_pred)
 
   test_out = pd.read_csv(r'data/test_result.csv')
   test_out = test_out.drop(del_column, axis=1)
   test_out = test_out.drop(test_out.index[0])
   test_out = test_out.to_numpy()
   y = []
-  for x in test_out:
-    y.append(x[0])
-  print(y)
+  err = []
+  for i in range(len(test_out)):
+    err.append(abs(test_out[i][0]-Y_pred[i]))
+    y.append(test_out[i][0])
+  print('actual = ', y)
+  print('err = ', err, ' : ', sum(err))
+  
+  xx = range(len(err))
+  plt.plot(xx, Y_pred, xx, y, xx, err)
+  plt.legend(['predict', 'actual', 'error'])
+  plt.show()
